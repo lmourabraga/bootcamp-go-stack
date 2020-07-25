0b9cbf87f9ffd977ae33756cd74fb456a4e952be
Luã Braga <ldemourabraga@gmail.com>
2020-07-25 19:52:13 -0300

    Page structure done with tooltips

--- 06-gobarber-web/src/components/Input/styles.ts
+++ 06-gobarber-web/src/components/Input/styles.ts
@@ -1,19 +1,48 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+import Tooltip from '../Tooltip';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
   max-width: 305px;
   display: flex;
   align-items: center;
+
   color: #666360;
+  border: 2px solid #232129;
 
   & + div {
     margin-top: 8px;
   }
+
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
   input {
     flex: 1;
     border: 0;
@@ -26,5 +55,23 @@ export const Container = styled.div`
   }
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  margin-left: 16px;
+  height: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
--- 06-gobarber-web/src/pages/SignIn/index.tsx
+++ 06-gobarber-web/src/pages/SignIn/index.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -8,13 +13,36 @@ import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => (
-  <>
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email is required')
+          .email('Type a valid email'),
+        password: Yup.string().required('Password required'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
     <Container>
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Enter to login</h1>
 
           <Input name="email" icon={FiMail} placeholder="Email" />
@@ -29,7 +57,7 @@ const SignIn: React.FC = () => (
           <Button type="submit">Sign in</Button>
 
           <a href="forgot">Forget the password?</a>
-        </form>
+        </Form>
 
         <a href="signUp">
           <FiLogIn />
@@ -39,7 +67,7 @@ const SignIn: React.FC = () => (
 
       <Background />
     </Container>
-  </>
-);
+  );
+};
 
 export default SignIn;
