51773235b6287973c313439bdcfc641f6e0c928c
Luã Braga <ldemourabraga@gmail.com>
2020-08-10 22:55:23 -0300

    working with input and button

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
     <Title>Faça seu logon</Title>
+
+    <Input />
+    <Input />
+
+    <Button>Entrar</Button>
   </Container>
 );
 
