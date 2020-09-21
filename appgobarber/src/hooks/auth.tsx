add1c10ee94e9850ff3965e5c613dd6cc71b5591
Luã Braga <ldemourabraga@gmail.com>
2020-09-20 23:04:19 -0300

    Rotas privadas

--- appgobarber/src/hooks/auth.tsx
+++ appgobarber/src/hooks/auth.tsx
@@ -15,6 +15,7 @@ interface AuthState {
 
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -23,6 +24,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData() {
@@ -31,6 +33,8 @@ const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) })
       }
+
+      setLoading(false);
     }
 
     loadStoragedData();
@@ -59,7 +63,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
