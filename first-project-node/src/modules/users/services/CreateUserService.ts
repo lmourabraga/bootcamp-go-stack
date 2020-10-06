7232edf72f19cae8ffdf735c9e932e65543153d2
Luã Braga <ldemourabraga@gmail.com>
2020-10-05 23:57:36 -0300

    Testando autenticação

--- first-project-node/src/modules/users/services/CreateUserService.ts
+++ first-project-node/src/modules/users/services/CreateUserService.ts
@@ -1,7 +1,6 @@
-import { hash } from 'bcryptjs';
-
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 import { injectable, inject } from 'tsyringe';
 
@@ -19,6 +18,9 @@ class CreateUserService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('HashProvider')
+        private hashProvider: IHashProvider,
     ) { }
 
     public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -29,7 +31,7 @@ class CreateUserService {
             throw new AppError('Email address already used.');
         }
 
-        const hashedPassword = await hash(password, 8);
+        const hashedPassword = await this.hashProvider.generateHash(password);
 
         const user = await this.usersRepository.create({
             name,
