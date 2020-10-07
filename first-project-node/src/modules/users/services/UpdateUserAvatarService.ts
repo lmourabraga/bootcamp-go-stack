6e921667ee3ca06aaeecaebd2a568c2172942e9c
Luã Braga <ldemourabraga@gmail.com>
2020-10-06 22:41:28 -0300

    Provider de storage

--- first-project-node/src/modules/users/services/UpdateUserAvatarService.ts
+++ first-project-node/src/modules/users/services/UpdateUserAvatarService.ts
@@ -6,6 +6,7 @@ import User from '../infra/typeorm/entities/User';
 
 import IUsersRepository from '../repositories/IUsersRepository';
 import AppError from '@shared/errors/AppError';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 import { injectable, inject } from 'tsyringe';
 
@@ -20,6 +21,9 @@ class UpdateUserAvatarService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('StorageProvider')
+        private storageProvider: IStorageProvider,
     ) { }
 
 
@@ -35,20 +39,12 @@ class UpdateUserAvatarService {
         }
 
         if (user.avatar) {
-            const userAvatarFilePath = path.join(
-                uploadConfig.directory,
-                user.avatar,
-            );
-            const userAvatarFileExists = await fs.promises.stat(
-                userAvatarFilePath,
-            );
-
-            if (userAvatarFileExists) {
-                await fs.promises.unlink(userAvatarFilePath);
-            }
+            await this.storageProvider.deleteFile(user.avatar);
         }
 
-        user.avatar = avatarFilename;
+        const filename = await this.storageProvider.saveFile(avatarFilename);
+
+        user.avatar = filename;
 
         await this.usersRepository.save(user);
 
